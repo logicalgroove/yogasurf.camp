1b9f131ec1febf057c33db67a6011890ddda56c3
Alex Lopez <webodessa@gmail.com>
2015-03-13 11:35:36 +0200

    initial commit

---
+++ strtp/main.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('.header-9-sub .background').parallax('20%', 0.3, true)
+  $('.header-18 .background').parallax('50%', 0.3, true)
+  $('.content-23').each(function(){
+    $(this).parallax('50%', 0.3, true);
+  });
+});
